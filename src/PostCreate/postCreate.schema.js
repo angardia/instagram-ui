13f7b66ff43f34d0c2d69f91105a9559c9a6e020
angardia <37413438+angardia@users.noreply.github.com>
2021-04-05 23:46:23 +0300

    style: change error msg description

--- src/PostCreate/postCreate.schema.js
+++ src/PostCreate/postCreate.schema.js
@@ -1,6 +1,6 @@
 import * as yup from "yup";
 
 export const createPostSchema = yup.object().shape({
-image: yup.mixed().required("image is req"),
+image: yup.mixed().required("Image required"),
 description: yup.string().max(2000, " desc")
 });
